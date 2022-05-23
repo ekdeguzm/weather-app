306f657109d58395e9a7c3c10bdd256a405830b5
Kawayan <kawayan.deguzman@gmail.com>
2022-05-22 23:34:32 -0700

    add apiKey parameter and seperate into multiple lines

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let weather = {
-  "apiKey": "e21c487737a00fcbbc4e19b710d3360c",
+  apiKey: "e21c487737a00fcbbc4e19b710d3360c",
   fetchWeather: function() {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=e21c487737a00fcbbc4e19b710d3360c",
+      "https://api.openweathermap.org/data/2.5/weather?q=" 
+      + city 
+      + "&units=metric&appid=" 
+      + apiKey,
     ).then((response) => response.json())
     .then((data) => console.log(data));
   }
