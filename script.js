0489c7045e8e1fbd27acd09f446cdebe81c57c53
Kawayan <kawayan.deguzman@gmail.com>
2022-05-22 23:32:44 -0700

    add parameter of city

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let weather = {
   "apiKey": "e21c487737a00fcbbc4e19b710d3360c",
   fetchWeather: function() {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=e21c487737a00fcbbc4e19b710d3360c"
+      "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=e21c487737a00fcbbc4e19b710d3360c",
     ).then((response) => response.json())
     .then((data) => console.log(data));
   }
