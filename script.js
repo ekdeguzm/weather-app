7ea4c39081c50a8525d7934c0c772e59d32fdb63
Kawayan <kawayan.deguzman@gmail.com>
2022-05-22 17:38:30 -0700

    create function that calls API

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ let weather = {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=e21c487737a00fcbbc4e19b710d3360c"
     ).then((response) => response.json())
-    .
+    .then((data) => console.log(data));
   }
 }
