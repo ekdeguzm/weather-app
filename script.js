c4764d4039ddc236af6c2a7190a47807a0255882
Kawayan <kawayan.deguzman@gmail.com>
2022-05-22 17:37:07 -0700

    enter in URL for API

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let weather = {
-  "apiKey": "e21c487737a00fcbbc4e19b710d3360c";
+  "apiKey": "e21c487737a00fcbbc4e19b710d3360c",
+  fetchWeather: function() {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=e21c487737a00fcbbc4e19b710d3360c"
+    ).then((response) => response.json())
+    .
+  }
 }
