6938a2b01fdadc9ed889b1ac5323820e3af6a3d2
Kawayan <kawayan.deguzman@gmail.com>
2022-05-23 23:55:21 -0700

    add displayWeather function

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ let weather = {
       "https://api.openweathermap.org/data/2.5/weather?q=" 
       + city 
       + "&units=metric&appid=" 
-      + apiKey,
-    ).then((response) => response.json())
-    .then((data) => console.log(data));
-  }
-}
+      + apiKey;
+    ),
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  },
+  displayWeather: function() 
+};
