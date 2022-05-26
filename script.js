6e9d003c3a11f5b9fdf53afb428512f90b1ce746
Kawayan <kawayan.deguzman@gmail.com>
2022-05-25 23:58:00 -0700

    have icon change depending on weather

--- script.js
+++ script.js
@@ -17,5 +17,8 @@ let weather = {
     const { speed } = data.wind;
     console.log(name,icon,description,temp,humidity,speed);
     document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"
+    + icon
+    + "@2x.png"
   }
 };
