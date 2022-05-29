2ef7a85c56001c94e31e4233743fcb8878865501
Kawayan <kawayan.deguzman@gmail.com>
2022-05-28 23:49:59 -0700

    make search bar work and change input into weather api

--- script.js
+++ script.js
@@ -24,5 +24,12 @@ let weather = {
     document.querySelector(".temp").innerText = temp + "°C"
     document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%"
     document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h"
+  },
+  search: function() {
+    this.fetchWeather(document.querySelector(".search-bar").value)
   }
 };
+
+document.querySelector(".search button").addEventListener("click", function() {
+  sear
+})
